022f530fc5155b1668fd0a305e3407e59e2cd597
dawnawll365 <459890570@qq.com>
2015-06-08 03:32:20 +0800

    figure

---
+++ figure/plot1.R
@@ -0,0 +1,22 @@
+setwd("D:/My RDocuments/coursera/cour4project1")
+house_power <- read.table("D:/My RDocuments/coursera/cour4project1/household_power_consumption.txt",header = TRUE,sep = ';')
+#house_power$Time <- strptime(house_power$Time,"%H:%M:%S")
+house_power$Date <- as.Date(house_power$Date,"%d/%m/%Y")
+str(house_power)
+head(house_power)
+tail(house_power)
+from = as.Date("2007-02-01", format="%Y-%m-%d")
+to = as.Date("2007-02-02", format="%Y-%m-%d")
+a <- strptime("2007-02-02 23:21:00","%Y-%m-%d %H:%M:%S")
+house_power_sub <-house_power[((house_power$Date >= from) & (house_power$Date <= to)),]
+head(house_power_sub)
+str(house_power_sub)
+## Manipulate the hpc data
+x <- strptime(paste(house_power_sub[,1], house_power_sub [,2]), format = "%Y-%m-%d %H:%M:%S")
+house_power_sub <-house_power_sub[,-2]
+house_power_sub[,1] <-as.POSIXct(x)
+house_power_sub[,2] <- as.numeric(house_power_sub[,2])
+# plot 1
+png(filename="plot1.png", width=480, height=480)
+hist(house_power_sub[,2]/1000, main="Global Active Power", xlab="Global Active Power(kilowatts)", ylab="Frequency", col="red")
+dev.off()
---
+++ figure/plot3.R
@@ -0,0 +1,30 @@
+setwd("D:/My RDocuments/coursera/cour4project1")
+house_power <- read.table("D:/My RDocuments/coursera/cour4project1/household_power_consumption.txt",header = TRUE,sep = ';')
+#house_power$Time <- strptime(house_power$Time,"%H:%M:%S")
+house_power$Date <- as.Date(house_power$Date,"%d/%m/%Y")
+str(house_power)
+head(house_power)
+tail(house_power)
+from = as.Date("2007-02-01", format="%Y-%m-%d")
+to = as.Date("2007-02-02", format="%Y-%m-%d")
+a <- strptime("2007-02-02 23:21:00","%Y-%m-%d %H:%M:%S")
+house_power_sub <-house_power[((house_power$Date >= from) & (house_power$Date <= to)),]
+head(house_power_sub)
+str(house_power_sub)
+## Manipulate the hpc data
+x <- strptime(paste(house_power_sub[,1], house_power_sub [,2]), format = "%Y-%m-%d %H:%M:%S")
+house_power_sub <-house_power_sub[,-2]
+house_power_sub[,1] <-as.POSIXct(x)
+house_power_sub[,2] <- as.numeric(house_power_sub[,2])
+#plot 3
+
+house_power_sub[,6] <- as.numeric(house_power_sub[,6])
+house_power_sub[,7] <- as.numeric(house_power_sub[,7])
+house_power_sub[,8] <- as.numeric(house_power_sub[,8])
+png(filename="plot3.png", width=480, height=480)
+plot(house_power_sub[,1],house_power_sub[,6], xlab="", ylab="Energy sub metering", type="n")
+lines(house_power_sub[,1], house_power_sub[,6], type="l")
+lines(house_power_sub[,1], house_power_sub[,7], type="l", col="red")
+lines(house_power_sub[,1], house_power_sub[,8], type="l", col="blue")
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=c(1,1,1), col=c("black", "red", "blue"), cex=0.9)
+dev.off()
